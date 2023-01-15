b41b43de6eaaf17247c63ecf13c6fe94ad47f456
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-15 16:40:42 +0300

    Example_010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 13, 48, 798, 65, 25, 46, 27, 464, 25 };
+
+int n = array.Length;
+int find = 25;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
