9d74e7fad40bde38d47e1c078e8b67c33699289c
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-19 21:09:06 +0300

    Add Methods

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿// Ничего не принимает, ничего не возвращает
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+// Method1();
+
+// что-то принимают, ничего не возвращают
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method2("Привет", 3); // или Method2(count: 3, msg: "Привет");
+
+// ничего не принимают, что-то возвращают
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+//int year = Method3();
+//Console.WriteLine(year);
+
+// что-то принимают, что-то возвращают
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(3, "Га");
+Console.WriteLine($"Гуси: {res}");
