f1e70b1dd69ac442b4925b6fb4f8691cc105045e
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-02-21 09:06:41 +0300

    Add NumbersRecursion

---
+++ Example021_NumbersRecursion/Program.cs
@@ -0,0 +1,19 @@
+﻿// Собрать строку с числами от a до b, a <= b
+
+// string NumbersRec(int a, int b)
+// {
+//     if (a < b) return $"{a} " + NumbersRec(a + 1, b);
+//     else return $"{b}";
+// }
+
+// Console.Write(NumbersRec(10, 1));
+
+// Собрать строку с числами от b до a, a >= b
+
+string NumbersRec(int a, int b)
+{
+    if (b < a) return NumbersRec(a, b + 1) + $"{b} ";
+    else return $"{a} ";
+}
+
+Console.Write(NumbersRec(10, 1));
